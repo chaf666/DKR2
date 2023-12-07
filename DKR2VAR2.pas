b9d31935e5ef21f264bd614e873e89fad5e48fd9
chaf666 <144238017+chaf666@users.noreply.github.com>
2023-12-07 16:59:07 +0300

    Add files via upload

---
+++ DKR2VAR2.pas
@@ -0,0 +1,17 @@
+﻿program DKR2VAR2;
+var A: array of integer;
+ n,i :integer; f:boolean;
+ begin 
+   writeln('Введите длину массива: ');
+   readln(n);
+   setlength(A,n);
+   writeln('Введите элементы массива: ');
+   for i:=0 to n-1 do begin
+   readln(A[i]);
+     if A[i]<>(i+1)*(i+1) then begin
+       f:=true;
+       break
+     end;
+   end;
+   if f then writeln('Заданный массив не имеет параболическую форму') else writeln('Заданный массив имеет параболическую форму');
+ end.
---
+++ DKRVAR2zad2.pas
@@ -0,0 +1,26 @@
+﻿program DKR2VAR2zad2;
+var w: string;
+a,b,i: integer;
+begin
+  a:= 0;
+  b:= 0;
+  
+  writeln('Введите строку:');
+  readln(w);
+  
+  for i := 1 to length(w) do
+  begin
+    if (w[i] = ' ') or (i = length(w)) then
+    begin
+      if (i = length(w)) and (w[i] <> ' ') then
+        b:= b + 1;
+      
+      a:= a + 1;
+    end
+    else
+      b:= b + 1;
+  end;
+  
+  writeln('Количество слов в строке: ', a);
+  writeln('Количество букв в строке: ', b);
+end.
